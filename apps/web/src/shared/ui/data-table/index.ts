b7c70228b11e1d1b2884772fded7ed516d656d0a
Logan Honeycutt <admin@kzn.sh>
2026-08-25 15:33:28 -0400

    feat(web): add apps/web/src/shared/ui/data-table/index.ts

---
+++ apps/web/src/shared/ui/data-table/index.ts
@@ -0,0 +1,18 @@
+export { DataTable } from "./data-table";
+export { DataTableColumnHeader } from "./data-table-column-header";
+export { DataTablePagination } from "./data-table-pagination";
+export { DataTableViewOptions } from "./data-table-view-options";
+export { EditableTextCell } from "./editable-text-cell";
+export { EditableSelectCell } from "./editable-select-cell";
+export type { EditableSelectOption } from "./editable-select-cell";
+export { EditableSuggestCell } from "./editable-suggest-cell";
+export type { EditableSuggestOption } from "./editable-suggest-cell";
+export {
+  DataTableAddRow,
+  DataTableComposerActions,
+  DataTableComposerRow,
+  TABLE_COMPOSER_INPUT_CLASS,
+  TableComposerInput,
+  tableComposerKeyDown,
+} from "./table-composer";
+export { useDataTable } from "./use-data-table";
