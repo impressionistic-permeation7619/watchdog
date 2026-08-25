ddaeea7898ffe5a5bf4429ec675a0253fbcdafee
Logan Honeycutt <admin@kzn.sh>
2026-08-25 15:33:08 -0400

    feat(log): add packages/log/src/index.ts

---
+++ packages/log/src/index.ts
@@ -0,0 +1,19 @@
+export type { AuditableLogger } from "evlog";
+export { createLogger } from "evlog";
+export { identifyUser, maskEmail } from "evlog/better-auth";
+
+export { initWatchdogLogger, type InitWatchdogLoggerOptions } from "./init";
+export {
+  getRequestLogger,
+  peekRequestLogger,
+  runWithRequestLogger,
+  storage,
+} from "./storage";
+export type {
+  AuthLogFields,
+  CapLogFields,
+  CaseLogFields,
+  JobLogFields,
+  UserLogFields,
+} from "./fields";
+export { jobWideEventFields } from "./fields";
