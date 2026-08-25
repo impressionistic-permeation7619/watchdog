11614822c53dfb43df5b78341d0c381b9efb33c7
Logan Honeycutt <admin@kzn.sh>
2026-08-25 15:33:13 -0400

    feat(api): add packages/api/src/index.ts

---
+++ packages/api/src/index.ts
@@ -0,0 +1,4 @@
+export type { ApiActor, ApiContext } from "./context";
+export { pub, authed } from "./os";
+export { generateOpenAPISpec } from "./openapi";
+export { router, type AppRouter } from "./router";
